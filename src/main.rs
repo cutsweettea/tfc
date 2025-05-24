9bebd5ef9286ce3aa6ae8b4fef8beeaacd40d296
cutsweettea <swagastan@proton.me>
2025-05-23 23:19:05 -0400

    added argument parser
    can now parse arguments in da command line with predefined arguments, argument types (NOT VARIABLE TYPES), yay

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+mod argp;
+
+use argp::argparse::{Argument, ArgumentParser};
+
+use std::env;
+
+pub use crate::argp::argparse;
+
 fn main() {
-    println!("hey thereee");
+    let argp = ArgumentParser {
+        argsvec: env::args().collect(),
+        prefix: '-',
+        args: &[
+            Argument {
+                trigger: "input",
+                mtrigger: "i",
+                isflag: false,
+                isrequired: true
+            },
+            Argument {
+                trigger: "output",
+                mtrigger: "o",
+                isflag: false,
+                isrequired: true
+            },
+            Argument {
+                trigger: "verbose",
+                mtrigger: "v",
+                isflag: true,
+                isrequired: false
+            }
+        ]
+    };
+
+    let fa = argp.compile();
+    for arg in fa.args.iter().clone() {
+        if arg.arg.isflag { println!("flag: {}", format!("{}, {}", arg.arg.trigger, arg.flagged)); }
+        else { println!("val: {}", format!("{}, {}", arg.arg.trigger, arg.val)); }
+    }
 }
