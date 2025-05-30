149c6e7e72a82e456828a0753d8046c62e94d7ca
cutsweettea <swagastan@proton.me>
2025-05-30 15:49:36 -0400

    more arg parse
    argument parsers can now be named and described, arguments now have labels, and everything is right in the world

--- src/argp.rs
+++ src/argp.rs
@@ -1,50 +1,73 @@
 pub mod argparse {
+    use std::collections::HashMap;
+    use std::process::exit;
+    use itertools::Itertools;
+
     pub struct Argument {
         pub trigger: &'static str,
         pub mtrigger: &'static str,
         pub isflag: bool,
         pub isrequired: bool,
+        pub help: &'static str
     }
 
     pub struct ArgumentParser { 
         pub argsvec: Vec<String>,
         pub prefix: char,
+        pub name: &'static str,
+        pub desc: &'static str,
         pub args: &'static [Argument]
     }
 
     impl ArgumentParser {
         pub fn compile(&self) -> FinalizedArguments {
+            // check for predefined help and add if doesn't exist
+            let mut newargs: Vec<&Argument> = vec![];
+            let help_arg = &Argument { trigger: "help", mtrigger: "h", isflag: true, isrequired: false, help: "sends help" };
+            newargs.push(help_arg);
+
+            // panicking when help or h is used as a trigger or mini trigger so ppl know why its failing
+            for arg in self.args {
+                if arg.trigger == help_arg.trigger || arg.trigger == help_arg.mtrigger { panic!("{}", format!("{} uses {} as trigger, which is reserved (you cannot use help / h for any trigger or mini trigger)", arg.trigger, help_arg.trigger)) }
+                else if arg.mtrigger == help_arg.trigger || arg.mtrigger == help_arg.mtrigger { panic!("{}", format!("{} uses {} as mtrigger, which is reserved (you cannot use help / h for any trigger or mini trigger)", arg.trigger, help_arg.trigger)) }
+                newargs.push(arg);
+            }
+
+            // checks for redefinitions of programmers arguments' triggers or mini triggers
             let mut fargs: Vec<FinalizedArgument> = vec![];
             let mut reading: bool = false;
-            let mut current: &'static Argument = &Argument { trigger: "", mtrigger: "", isflag: false, isrequired: false };
+            let mut current: &'static Argument = &Argument { trigger: "", mtrigger: "", isflag: false, isrequired: false, help: "" };
             let mut current_val: String = "".to_string();
             let mut i: usize = 0;
 
             let mut _trigs: Vec<String> = vec![];
             let mut _mtrigs: Vec<String> = vec![];
             for arg in self.args {
-                if _trigs.contains(&arg.trigger.to_string()) { panic!("{}", format!("FATAL redefinition of '{}' / '{}'", arg.trigger, arg.mtrigger)) }
-                if _mtrigs.contains(&arg.mtrigger.to_string()) { panic!("{}", format!("FATAL redefinition of '{}' / '{}'", arg.trigger, arg.mtrigger)) }
+                if _trigs.contains(&arg.trigger.to_string()) || _trigs.contains(&arg.mtrigger.to_string()) { panic!("{}", format!("FATAL redefinition of '{}' / '{}'", arg.trigger, arg.mtrigger)) }
+                if _mtrigs.contains(&arg.mtrigger.to_string()) || _mtrigs.contains(&arg.trigger.to_string()) { panic!("{}", format!("FATAL redefinition of '{}' / '{}'", arg.trigger, arg.mtrigger)) }
                 _trigs.push(arg.trigger.to_string());
                 _mtrigs.push(arg.mtrigger.to_string());
             }
 
             for a in self.get_args() {
                 i += 1;
+                // set astr to the current argument without the prefix(s)
                 let aspl: Vec<_> = a.split(self.prefix).collect();
                 let astr: &str = aspl[aspl.len()-1];
+
                 if reading {
                     if reading && a.starts_with(self.prefix) { 
-                        if current_val.is_empty() { panic!("{}", format!("cannot define new variable ('{}') before defining the variable before ('{}')", a, current.trigger)); }
+                        // starts redefinition of new argument after encountering prefix during reading
+                        if current_val.is_empty() { panic!("{}", format!("cannot define new variable '{}' before defining the variable before '{}'", a, current.trigger)); }
                         fargs.push(FinalizedArgument { arg: current, val: current_val.clone().trim().to_string(), flagged: false });
-                        current = &Argument { trigger: "", mtrigger: "", isflag: false, isrequired: false };
+                        current = &Argument { trigger: "", mtrigger: "", isflag: false, isrequired: false, help: "" };
                         if current_val.is_empty() { panic!("FATAL attempting to add null value for '{}'", a); }
 
                         current_val = "".to_string();
                         reading = false;
 
                         let mut found = false;
-                        for arg in self.args {
+                        for arg in newargs.clone() {
                             if (arg.trigger == astr && aspl.len() == 3) || (arg.mtrigger == astr && aspl.len() == 2) {
                                 current = arg;
                                 found = true;
@@ -72,12 +95,13 @@ pub mod argparse {
                     if !found { panic!("FATAL trigger '{}' not found", a); }
                 }
 
-                if i == self.get_args().len() { 
+                if i == self.get_args().len() {
                     if current_val.is_empty() && !current.isflag { panic!("attempting to add null value for '{}'", a); }
                     else if !current.isflag { fargs.push(FinalizedArgument { arg: current, val: current_val.clone().trim().to_string(), flagged: false }); }
                 }
             }
 
+            // checks for redefinitions of users arguments' triggers or mini triggers
             let mut _utrigs: Vec<String> = vec![];
             let mut _umtrigs: Vec<String> = vec![];
             for arg in fargs.iter().clone() {
@@ -87,13 +111,14 @@ pub mod argparse {
                 _umtrigs.push(arg.arg.mtrigger.to_string());
             }
 
+            // checks for missing required arguments and adds unflagged arguments as false
             let mut missing: Vec<&str> = vec![];
             for a in self.args {
                 let mut found = false;
                 for fa in fargs.iter().clone() {
                     if fa.arg.trigger == a.trigger && fa.arg.mtrigger == a.mtrigger { 
                         found = true;
-                        break;
+                        break; 
                     }
                 }
 
@@ -105,7 +130,36 @@ pub mod argparse {
             }
 
             if missing.len() > 0 { panic!("FATAL missing required argument(s): {}", missing.join(", ")) }
-            return FinalizedArguments { args: fargs }
+
+            // checks if someone needs help
+            let finalargs = FinalizedArguments { args: fargs };
+            match finalargs.get("help") {
+                Some(arg) => {
+                    if arg.flagged {
+                        println!("{}", self.name);
+                        println!("{}\n", self.desc);
+                        println!("arguments:");
+
+                        let mut stuff: HashMap<&str, (String, &str)> = HashMap::new();
+                        let mut longest_trig = 0;
+                        for arg in newargs {
+                            let trig = self.prefix.to_string() + &self.prefix.to_string() + &arg.trigger.to_string() + ", " + &self.prefix.to_string() + &arg.mtrigger.to_string();
+                            let triglen = trig.len();
+                            if triglen > longest_trig { longest_trig = triglen }
+                            stuff.insert(arg.trigger, (trig, arg.help));
+                        }
+
+                        for (_, stuff) in stuff.iter().sorted() {
+                            println!("{}", format!("{}{}{}{}", (0..2).map(|_| " ").collect::<String>(), stuff.0, (0..((longest_trig - stuff.0.len())+4)).map(|_| " ").collect::<String>(), stuff.1));
+                        }
+
+                        exit(0);
+                    }
+                }
+                None => {}
+            }
+
+            return finalargs;
         }
 
         fn get_args(&self) -> Vec<String> {
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
 extern crate ffmpeg_next as ffmpeg;
 
+mod argp;
+
+use argp::argparse::{Argument, ArgumentParser};
+
 use log::info;
+use log::LevelFilter;
+use std::env;
 
 use log4rs::append::console::ConsoleAppender;
 use log4rs::config::Config;
 use log4rs::config::Appender;
 use log4rs::config::runtime::Root;
-use log::LevelFilter;
 
 fn setup_log4rs() {
     let stdout = ConsoleAppender::builder().build();
@@ -19,5 +24,63 @@ fn setup_log4rs() {
 
 fn main() {
     setup_log4rs();
-    info!("testing");
+    let argp = ArgumentParser {
+        argsvec: env::args().collect(),
+        prefix: '-',
+        name: "tea",
+        desc: "tea goes from one pot to another, maybe something happened",
+        args: &[
+            Argument {
+                trigger: "input",
+                mtrigger: "i",
+                isflag: false,
+                isrequired: true,
+                help: "path to input file"
+            },
+            Argument {
+                trigger: "output",
+                mtrigger: "o",
+                isflag: false,
+                isrequired: true,
+                help: "path to output file"
+            },
+            Argument {
+                trigger: "verbose",
+                mtrigger: "v",
+                isflag: true,
+                isrequired: false,
+                help: "will print more debug stuff"
+            },
+            Argument {
+                trigger: "nopluh",
+                mtrigger: "np",
+                isflag: true,
+                isrequired: false,
+                help: "don't print the first line :("
+            }
+        ]
+    };
+
+    let fa = argp.compile();
+
+    match fa.get("nopluh") {
+        Some(arg) => {
+            if !arg.flagged { info!("pluh @umatterbro dc/ig/gh https://github.com/cutsweettea/tfc"); }
+        }
+        None => {}
+    }
+
+    let inf;
+    let outf;
+    match fa.get("input") {
+        Some(arg) => { inf = arg.val.clone(); },
+        None => { panic!("no input file specified"); }
+    }
+
+    match fa.get("output") {
+        Some(arg) => { outf =  arg.val.clone(); },
+        None => { panic!("no output file specified"); }
+    }
+
+    info!("{}", format!("converting input '{}' and writing output to '{}'", inf, outf));
 }
